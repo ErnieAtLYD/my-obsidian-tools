738d3c085d9500d36933ac538265c72699b9a6b3
Ernie Hsiung <e@erniehsiung.com>
2024-09-15 18:25:18 -0700

    fix(api): adjust maxDuration for serverless functions to comply with hobby plan limits

--- app/api/summarize/urls/scrape/route.tsx
+++ app/api/summarize/urls/scrape/route.tsx
@@ -4,7 +4,9 @@ import { RouteMessageMap } from '@/types/upstash'
 import { verifyUpstashSignature } from '@/utils/upstash'
 import { processUrl } from '@/utils/urls'
 
-export const maxDuration = 300
+// Serverless Functions must have a maxDuration between 1 and 60 for plan hobby.
+// export const maxDuration = 300
+export const maxDuration = 60
 
 export async function POST(req: NextRequest) {
   const body: RouteMessageMap['/api/summarize/urls/scrape'] =
